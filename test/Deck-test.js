de6726340cfdb130f3d25ff7b3f4f26b2d9419be
Hayley Witherell <hayleywitherell@gmail.com>
2021-07-05 16:25:32 -0600

    Add basic tests

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -4,4 +4,14 @@ const Deck = require('../src/Deck');
 
 describe('Turn', () => {
   
+  it('should be a function', () => {
+
+    expect(Turn).to.be.a('function');
+  });
+
+  it('should be an instance of Turn', () => {
+    const turn = new Turn();
+
+    expect(turn).to.be.an.instanceof(Turn);
+  }); 
 }
