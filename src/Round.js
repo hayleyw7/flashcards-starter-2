6a65f256d849c585ed0b1670a93a3c864511151c
Hayley Witherell <hayleywitherell@gmail.com>
2021-07-06 17:47:45 -0600

    Add Round class

--- src/Round.js
+++ src/Round.js
@@ -1,6 +1,39 @@
-class Round {
-  constructor() {
+const Turn = require('./Turn');
 
+class Round {
+  constructor(deck) {
+    this.deck = deck;
+    this.turns = 0;
+    this.incorrectGuesses = [];
+    this.currentCard = this.deck.cards[this.turns];
+  }
+
+  returnCurrentCard() {
+    return this.currentCard;
+  }
+
+  takeTurn(guess) {
+    this.turns++;
+    const turn = new Turn (guess, this.currentCard);
+    this.currentCard = this.deck.cards[this.turns];
+
+    if (!turn.evaluateGuess()) {
+      this.incorrectGuesses.push(this.currentCard.id);
+    }
+
+    turn.giveFeedback();
+    return turn.giveFeedback();
+  }
+
+  calculatePercentCorrect() {
+    const correctGuesses = this.turns - this.incorrectGuesses.length;
+    const percentCorrect = (correctGuesses / this.turns) * 100;
+    return percentCorrect;
+  }
+
+  endRound() {
+    console.log(`** Round over! ** You answered ` + this.calculatePercentCorrect() + `% of the questions correctly!`);
+    return `** Round over! ** You answered ` + this.calculatePercentCorrect() + `% of the questions correctly!`;
   }
 }
 
