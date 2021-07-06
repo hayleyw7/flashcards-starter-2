480cc4d550ff361330e6d0623cfda42f04e1bd21
Hayley Witherell <hayleywitherell@gmail.com>
2021-07-06 17:33:48 -0600

    Add Turn class

--- src/Turn.js
+++ src/Turn.js
@@ -1,6 +1,31 @@
 class Turn {
-  constructor() {
+  constructor(guess, card) {
+    this.guess = guess;
+    this.currentCard = card;
+  }
+  
+  returnGuess() {
+    return this.guess;
+  }
 
+  returnCard() {
+    return this.currentCard;
+  }
+
+  evaluateGuess() {
+    if (this.guess === this.currentCard.correctAnswer) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  giveFeedback() {
+    if (!this.evaluateGuess()) {
+      return `incorrect!`;
+    } else {
+      return `correct!`;
+    }
   }
 }
 
