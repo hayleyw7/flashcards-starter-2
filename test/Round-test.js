f33e014b1c58fa23c7cffd4f88d7f8c8e8d044c4
Hayley Witherell <hayleywitherell@gmail.com>
2021-07-06 18:48:54 -0600

    Add takeTurn test for incorrect answer

--- test/Round-test.js
+++ test/Round-test.js
@@ -225,7 +225,7 @@ describe('Round', () => {
     expect(round.incorrectGuesses).to.deep.equal([card2.id, card3.id]);
   });
 
-  it('should provide feedback whether correct/incorrect', () => {
+  it('should provide feedback if correct', () => {
     
     const card1 = new Card(
       1,
@@ -252,6 +252,35 @@ describe('Round', () => {
     const round = new Round(deck);
 
     expect(round.takeTurn('sea otter')).to.deep.equal('correct!');
+  });
+
+    it('should provide feedback if incorrect', () => {
+    
+    const card1 = new Card(
+      1,
+      'What is Robbie\'s favorite animal',
+      ['sea otter', 'pug', 'capybara'],
+      'sea otter'
+    );
+
+    const card2 = new Card(
+      14,
+      'What organ is Khalid missing?',
+      ['spleen', 'appendix', 'gallbladder'],
+      'gallbladder'
+    );
+
+    const card3 = new Card(
+      12,
+      'What is Travis\'s favorite stress reliever?',
+      ['listening to music', 'watching Netflix', 'playing with bubble wrap'],
+      'playing with bubble wrap'
+    );
+
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+
+    expect(round.takeTurn('dog')).to.deep.equal('incorrect!');
   });
 
   it('should provide percent of correct guesses', () => {
